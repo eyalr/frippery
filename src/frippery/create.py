5293b754e26320d2e39128282afcd32a3b12852d
Nicole Zuckerman <nicolez@eventbrite.com>
2014-12-05 09:54:23 -0800

    Creating event and ticketclasses api calls

--- src/frippery/create.py
+++ src/frippery/create.py
@@ -1,18 +1,37 @@
+import datetime
 import json
-import requests as external_requests
+from flask import g
 
 
 def create_new_event(form_values):
-    json_blob = json.dumps(form_values)
-    response_from_api = external_requests.post("https://www.eventbrite.com/xml/events/composite-create/", json_blob)
-    import pdb; pdb.set_trace()
+    # user_id = g.user_id
+    # import pdb; pdb.set_trace()
+    organizer_id = g.eb_api.get('/v3/users/me/organizers')
+    start_date = form_values['event_start_date'] + "T" + form_values['event_start_time'] + "Z"
+    end_date = form_values['event_end_date'] + "T" + form_values['event_end_time'] + "Z"
+    create_data = {
+        'event.name.html': form_values['event_name'],
+        'event.currency': 'USD',
+        'event.description.html': form_values['event_description'],
+        'event.start.utc': start_date,
+        'event.start.timezone': 'America/Los_Angeles',
+        'event.end.utc': end_date,
+        'event.end.timezone': 'America/Los_Angeles',
+        'event.organizer_id': organizer_id,
+        'event.online_event': True,
+    }
+
+    response_from_api = g.eb_api.post("events/", create_data)
     event_id = response_from_api.eid
+    ticket_post_path = 'v3/events/' + event_id + "/ticket_classes"
+    ticket_post_information = {
+        ticket_class.name: g.frippery_app,
+        ticket_class.quantity_total: "",
+        ticket_class.free: True,
+    }
+    g.eb_api.post(ticket_post_path,)
     response_from_api = external_requests.post("https://www.eventbrite.com/xml/events/publish/", event_id)
 
-
-# figure out how to pass in:
-# api key with request to api
-# ^^^ use eventbriteapi that gets passed in - eyal
 # start/end timezones
 # ^^^ this is in "Olson Format": http://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 # ^^^ 3rd column from left
