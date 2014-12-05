3276c5ab434e0e7645ce2c4cdd7f976fba929d1c
Jay Chan <jay@eventbrite.com>
2014-12-04 16:12:00 -0800

    app detection

--- src/frippery/app.py
+++ src/frippery/app.py
@@ -1,15 +1,32 @@
-from flask import Flask
-from flask import redirect
-from flask import render_template
-from flask import request
+import re
+
+from flask import (
+    Flask,
+    g,
+    redirect,
+    render_template,
+    request,
+)
 
 import create
+
+
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+    request.host
+    if 'secret-santa' in request.host.lower():
+        g.frippery_app = 'secret-santa'
+    else:
+        g.frippery_app = 'tourney'
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 @app.route('/create', methods=['GET'])
 def create_view():
-    import pdb; pdb.set_trace()
     return render_template('create.html')
 
 @app.route('/publish', methods=['POST'])
@@ -18,5 +35,5 @@ def submit_new_event():
     return redirect('/events')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', debug=True)
 
