0686d544e768fde3eee1ad731d243a0dcc18d626
Jay Chan <jay@eventbrite.com>
2014-12-05 02:07:56 -0800

    refactor auth logic into auth.py

--- src/frippery/app.py
+++ src/frippery/app.py
@@ -10,61 +10,30 @@ from flask import (
     session,
     url_for,
 )
-from flask_oauthlib.client import OAuth, OAuthException
-from werkzeug import security
+from flask_oauthlib.client import OAuthException
 
+import auth
 import create
 import storage
 
 import settings
 
 app = Flask(__name__)
-app.secret_key = 'development'
-oauth = OAuth(app)
+eventbite_apis = auth.initialize(app)
 
-class _NOT_DEFINED(object):
-    pass
-
-eventbriteapi = None
-
-host_eb_apis = {
-    'tourney': oauth.remote_app(
-        'eventbrite-tourney',
-        consumer_key='JYUY5CEW6XFOIALPTA',
-        consumer_secret=settings.tourney_secret_key,
-        request_token_params={
-            'state': lambda: security.gen_salt(10)
-        },
-        base_url='https://www.eventbriteapi.com/v3/',
-        request_token_url=None,
-        access_token_method='POST',
-        access_token_url='https://www.eventbrite.com/oauth/token',
-        authorize_url='https://www.eventbrite.com/oauth/authorize',
-    ),
-    'secret-santa': oauth.remote_app(
-        'eventbrite-ss',
-        consumer_key='Z5HCI737VIWT2DRTAK',
-        consumer_secret=settings.secret_santa_secret_key,
-        request_token_params={
-            'state': lambda: security.gen_salt(10)
-        },
-        base_url='https://www.eventbriteapi.com/v3/',
-        request_token_url=None,
-        access_token_method='POST',
-        access_token_url='https://www.eventbrite.com/oauth/token',
-        authorize_url='https://www.eventbrite.com/oauth/authorize',
-    ),
-}
-for _api in host_eb_apis.values():
-    _api.tokengetter(lambda *args: session.get('eventbrite_token'))
+@app.before_request
+def before_request():
+    if 'secret-santa' in request.host.lower():
+        g.frippery_app = 'secret-santa'
+    else:
+        g.frippery_app = 'tourney'
+    g.eb_api = eventbite_apis[g.frippery_app]
 
 
 @app.route('/', methods=['GET'])
 def index():
-    if 'eventbrite_token' in session:
-        me = eventbriteapi.get('users/me')
-        return jsonify(me.data)
-    # this is where user lands to enter their form information
+    if auth.is_logged_in():
+        return redirect('/events')
     return render_template('index.html')
 
 '''
@@ -77,38 +46,34 @@ def test_data():
 
 @app.route('/login')
 def login():
-    return eventbriteapi.authorize(
-        callback=url_for('authorized', _external=True),
-    )
+    if auth.is_logged_in():
+        return redirect('/events')
+    return g.eb_api.authorize(callback=url_for('authorize', _external=True))
+
+@app.route('/authorize')
+def authorize():
+    auth_error = auth.authorize(g.eb_api)
+    if auth_error is None:
+        return redirect('/events')
+    else:
+        return auth_error
 
 @app.route('/events')
-def authorized():
-    resp = eventbriteapi.authorized_response()
-    if resp is None or isinstance(resp, OAuthException):
-        return 'Access denied: reason=%s error=%s' % (
-            request.args['error_reason'],
-            request.args['error_description']
-        )
-    session['eventbrite_token'] = (resp['access_token'], '')
-    me = eventbriteapi.get('users/me')
-    return jsonify(me.data)
-
-@app.before_request
-def before_request():
-    request.host
-    if 'secret-santa' in request.host.lower():
-        g.frippery_app = 'secret-santa'
-    else:
-        g.frippery_app = 'tourney'
-    global eventbriteapi
-    eventbriteapi = host_eb_apis[g.frippery_app]
+def events():
+    if not auth.is_logged_in():
+        return redirect('/')
+    return "hello"
 
 @app.route('/create', methods=['GET'])
 def create_view():
+    if not auth.is_logged_in():
+        return redirect('/')
     return render_template('create.html')
 
 @app.route('/publish', methods=['POST'])
 def submit_new_event():
+    if not auth.is_logged_in():
+        return redirect('/')
     create.create_new_event(request.values.to_dict())
     return redirect('/events')
 
---
+++ src/frippery/auth.py
@@ -0,0 +1,76 @@
+from flask import (
+    g,
+    jsonify,
+    request,
+    session,
+)
+from flask_oauthlib.client import OAuth, OAuthException
+from werkzeug import security
+
+import settings
+
+def initialize(app):
+    app.secret_key = 'development'
+    oauth = OAuth(app)
+
+    host_eb_apis = {
+        'tourney': oauth.remote_app(
+            'eventbrite-tourney',
+            consumer_key='JYUY5CEW6XFOIALPTA',
+            consumer_secret=settings.tourney_secret_key,
+            request_token_params={
+                'state': lambda: security.gen_salt(10)
+            },
+            base_url='https://www.eventbriteapi.com/v3/',
+            request_token_url=None,
+            access_token_method='POST',
+            access_token_url='https://www.eventbrite.com/oauth/token',
+            authorize_url='https://www.eventbrite.com/oauth/authorize',
+        ),
+        'secret-santa': oauth.remote_app(
+            'eventbrite-ss',
+            consumer_key='Z5HCI737VIWT2DRTAK',
+            consumer_secret=settings.secret_santa_secret_key,
+            request_token_params={
+                'state': lambda: security.gen_salt(10)
+            },
+            base_url='https://www.eventbriteapi.com/v3/',
+            request_token_url=None,
+            access_token_method='POST',
+            access_token_url='https://www.eventbrite.com/oauth/token',
+            authorize_url='https://www.eventbrite.com/oauth/authorize',
+        ),
+    }
+    for _api in host_eb_apis.values():
+        _api.tokengetter(lambda *args: session.get('eventbrite_token'))
+
+    return host_eb_apis
+
+def authorize(eb_api):
+    resp = eb_api.authorized_response()
+    if resp is None or isinstance(resp, OAuthException):
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description'],
+        )
+    session['eventbrite_token'] = (resp['access_token'], '')
+    session['eventbrite_me'] = eb_api.get('users/me').data
+    ''' users/me data format
+        {
+        "emails": [
+            {
+            "email": "eyal+jay+mica+nicolez@eventbrite.com",
+            "primary": true,
+            "verified": false
+            }
+        ],
+        "first_name": null,
+        "id": "131185252373",
+        "last_name": null,
+        "name": "eyal+jay+mica+nicolez@eventbrite.com"
+        }
+    '''
+    return None
+
+def is_logged_in():
+    return 'eventbrite_token' in session
