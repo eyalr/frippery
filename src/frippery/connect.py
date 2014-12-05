6d4d6739bd0f60da17113ab53b0e9b098509c837
Jay Chan <jay@eventbrite.com>
2014-12-05 10:46:09 -0800

    2.6 compatible connect.py

--- src/frippery/connect.py
+++ src/frippery/connect.py
@@ -4,10 +4,10 @@ def connect_event(event_id):
     ticket_data = g.eb_api.get(
         'events/%d/ticket_classes' % (int(event_id),)
     ).data['ticket_classes']
-    ticket_classes = {
-        d['id']: {
+    ticket_classes = dict(
+        (d['id'], {
             'name': d['name'],
             'quantity_sold': d['quantity_sold'],
-        } for d in ticket_data
-    }
+        }) for d in ticket_data
+    )
     return ticket_classes
