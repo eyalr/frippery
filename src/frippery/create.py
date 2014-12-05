28c1acc874eaa294e7ba1d73fc455376c88dd539
Eyal Reuveni <eyal@eventbrite.com>
2014-12-04 22:29:00 -0800

    Figured out how to create an event! hurrah!

--- src/frippery/create.py
+++ src/frippery/create.py
@@ -12,5 +12,27 @@ def create_new_event(form_values):
 
 # figure out how to pass in:
 # api key with request to api
+# ^^^ use eventbriteapi that gets passed in - eyal
 # start/end timezones
+# ^^^ this is in "Olson Format": http://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+# ^^^ 3rd column from left
 # user id/authorization of some sort
+# ^^^ use eventbriteapi that gets passed in - eyal
+
+""" eyal: I succesfully created an event like so...
+created_event = eventbriteapi.post(
+    'events/',
+    {
+        'event.name.html': 'testing',
+        'event.currency': 'USD',
+        'event.description.html': 'test_description',
+        'event.start.utc': '2014-12-18T16:00:00Z',
+        'event.start.timezone': 'America/Los_Angeles',
+        'event.end.utc': '2014-12-18T23:00:00Z',
+        'event.end.timezone': 'America/Los_Angeles',
+        'event.organizer_id': '7803975089',
+        'event.online_event': True,
+    },
+)
+my_new_event_id = created_event['id']
+"""
