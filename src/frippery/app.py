445b234479ed6d0fbbb70916b15a960dc3db95c5
Eyal Reuveni <eyal@eventbrite.com>
2014-12-04 16:11:57 -0800

    Add eventbrite oauth to app.

--- src/frippery/app.py
+++ src/frippery/app.py
@@ -2,16 +2,64 @@ import re
 
 from flask import (
     Flask,
+    jsonify,
     g,
     redirect,
     render_template,
     request,
+    session,
+    url_for,
 )
+from flask_oauthlib.client import OAuth, OAuthException
+from werkzeug import security
 
 import create
 
-
 app = Flask(__name__)
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+eventbriteapi = oauth.remote_app(
+    'eventbrite',
+    consumer_key='Z5DXTVZGHJJV5GX5VJ',
+    consumer_secret='RHZHLC7BJTDB26IYBUEHZZ3EFAUAMY6UH33XVAPYPT7D5ANVHA',
+    request_token_params={
+        'state': lambda: security.gen_salt(10)
+    },
+    base_url='https://www.eventbriteapi.com/v3/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://www.eventbrite.com/oauth/token',
+    authorize_url='https://www.eventbrite.com/oauth/authorize'
+)
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    if 'eventbrite_token' in session:
+        me = eventbriteapi.get('users/me')
+        return jsonify(me.data)
+    # this is where user lands to enter their form information
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return eventbriteapi.authorize(
+        callback=url_for('authorized', _external=True),
+    )
+
+@app.route('/events')
+def authorized():
+    resp = eventbriteapi.authorized_response()
+    if resp is None or isinstance(resp, OAuthException):
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['eventbrite_token'] = (resp['access_token'], '')
+    me = eventbriteapi.get('users/me')
+    return jsonify(me.data)
 
 @app.before_request
 def before_request():
@@ -21,9 +69,9 @@ def before_request():
     else:
         g.frippery_app = 'tourney'
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
+@eventbriteapi.tokengetter
+def get_eventbrite_oauth_token():
+    return session.get('eventbrite_token')
 
 @app.route('/create', methods=['GET'])
 def create_view():
